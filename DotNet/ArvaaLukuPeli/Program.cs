f4eef13959d9ca7473d36a9ad56cb2f03364c5e9
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-09-23 14:47:48 +0300

    Aloitettu koodaamaan Arvaa luku -peliä.

---
+++ DotNet/ArvaaLukuPeli/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArvaaLukuPeli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tervetuloa Arvaa luku -peliin!");
+            
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Kiitos ajastasi, peli on päättynyt.");
+        }
+    }
+}
