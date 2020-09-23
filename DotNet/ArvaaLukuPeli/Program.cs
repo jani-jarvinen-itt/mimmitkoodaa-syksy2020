ee65ed23912e12275ec9ee0435283caea7402460
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-09-23 15:02:41 +0300

    Lisätty pelin peruslogiikka.

--- DotNet/ArvaaLukuPeli/Program.cs
+++ DotNet/ArvaaLukuPeli/Program.cs
@@ -11,11 +11,25 @@ namespace ArvaaLukuPeli
             // arvotaan satunnainen luku, josta tulee pelin oikea vastaus
             Random satunnainen = new Random();
             int oikeaLuku = satunnainen.Next(1, 21);
-            Console.WriteLine(oikeaLuku);
+            // Console.WriteLine(oikeaLuku);
 
+            // annetaan pelaajalle kolme arvauskertaa
+            int arvauskertoja = 0;
+            while (arvauskertoja < 3) {
 
+               Console.WriteLine("Anna arvauksesi välillä 1-20:");
+               int arvaus = int.Parse(Console.ReadLine());
 
-
+               if (arvaus < oikeaLuku) {
+                   Console.WriteLine("Oikea luku on suurempi!");
+               }
+               else if (arvaus > oikeaLuku) {
+                   Console.WriteLine("Oikea luku on pienempi!");
+               }
+               else {
+                   Console.WriteLine("Oikein! Voitit pelin!");
+               }
+            }
 
             Console.WriteLine("Kiitos ajastasi, peli on päättynyt.");
         }
