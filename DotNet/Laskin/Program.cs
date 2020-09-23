e20f4abb09f1f8c1ba661b6738fac6e0c6134d39
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-09-23 13:32:42 +0300

    Lisätty C#-kielisiä esimerkkisovelluksia: HelloWorld sekä Laskin.

---
+++ DotNet/Laskin/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna luku 1:");
+            string luku1 = Console.ReadLine();
+
+            Console.WriteLine("Anna luku 2:");
+            string luku2 = Console.ReadLine();
+
+            int summa = int.Parse(luku1) + int.Parse(luku2);
+            Console.WriteLine("Summa on: " + summa); 
+        }
+    }
+}
