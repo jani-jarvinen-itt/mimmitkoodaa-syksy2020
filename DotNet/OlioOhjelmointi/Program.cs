0686e53450e3c69db2e69c82eb3848ec5b81fb6b
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-09-24 11:41:20 +0300

    Lisätty yksinkertainen olio-ohjelmoinnin esimerkki C#-kielellä. Esimerkkinä henkilöauto ominaisuuksineen ja toimintoineen.

---
+++ DotNet/OlioOhjelmointi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OlioOhjelmointi
+{
+    class Henkilöauto
+    {
+        public int Huippunopeus { get; set; }
+        public string Väri { get; set; }
+        public int OvienMäärä { get; set; }
+        public bool MoottoriKäynnissä { get; set; }
+
+        public void Käynnistä()
+        {
+            MoottoriKäynnissä = true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Henkilöauto oma = new Henkilöauto();
+            Henkilöauto naapurin = new Henkilöauto();
+
+            oma.Väri = "keltainen";
+            oma.Huippunopeus = 200;
+
+            naapurin.Väri = "harmaa";
+            naapurin.Huippunopeus = 180;
+
+            oma.Käynnistä();
+        }
+    }
+}
