5ba0ada7e4047e8e2e9f44bf4a506bb73b0ddce7
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-09-29 10:27:41 +0300

    Lisätty XML- ja JSON-esimerkit kuvitteelisen tuotetiedon tallentamiseen.

---
+++ JsonTesti.js
@@ -0,0 +1,22 @@
+console.log("Moi JavaScript!");
+
+var tuotteet = [
+    {
+        "Id": 1000,
+        "Nimi": "Punainen omena",
+        "Hinta": 2.5
+    },
+    {
+        "Id": 1010,
+        "Nimi": "Mandariini",
+        "Hinta": 1.8
+    }
+];
+
+var omena = {
+    "Id": 1000,
+    "Nimi": "Punainen omena",
+    "Hinta": 2.5
+};
+
+console.log(tuotteet[0].Nimi);
