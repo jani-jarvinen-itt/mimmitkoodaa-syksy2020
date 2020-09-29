f5bb9002c1fe278e5aa7e5eabb09418e8bdc8ff3
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-09-29 10:37:10 +0300

    Lisätty Tuotteet-luokka sekä tätä varten yksinkertainen API-metodi.

--- DotNet/AspNetDemo/Controllers/TuotteetController.cs
+++ DotNet/AspNetDemo/Controllers/TuotteetController.cs
@@ -7,13 +7,47 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetDemo.Controllers
 {
+    public class Tuote
+    {
+        public int Id { get; set; }
+
+        public string Nimi { get; set; }
+
+        public double Hinta { get; set; }
+    }
+
+
     [Route("api/[controller]")]
     [ApiController]
     public class TuotteetController : ControllerBase
     {
+        public List<Tuote> KaikkiTuotteet()
+        {
+            List<Tuote> tuotteet = new List<Tuote>()
+            {
+                new Tuote()
+                {
+                    Id = 1000,
+                    Nimi = "Punainen omena",
+                    Hinta = 2.5
+                },
+                new Tuote()
+                {
+                    Id = 1010,
+                    Nimi = "Mandariini",
+                    Hinta = 1.8
+                }
+            };
+
+            return tuotteet;
+        }
+
+
+        /*
         public int Luku()
         {
             return 123;
         }
+        */
     }
 }
