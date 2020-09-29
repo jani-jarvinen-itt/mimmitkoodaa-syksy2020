e24ee53f92226aed6c9557995ab1081084b8c78b
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-09-29 10:28:18 +0300

    Aloitettu lisäämään ASP.NET-sovellukseen Web API -kontrolleria.

---
+++ DotNet/AspNetDemo/Controllers/TuotteetController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TuotteetController : ControllerBase
+    {
+        public int Luku()
+        {
+            return 123;
+        }
+    }
+}
